b67a19ff38985a1a8796843ae21e26b70141f7f8
areality <areality.dt@gmail.com>
2024-09-15 23:52:13 +0300

    Initial commit

---
+++ src/l11/task1.rs
@@ -0,0 +1,20 @@
+trait Action {
+    fn say(&self);
+}
+
+struct Person {
+    name: String,
+}
+
+impl Action for Person {
+    fn say(&self) {
+        println!("Hello, {}", self.name);
+    }
+}
+
+fn run() {
+    let person = Person {
+        name: String::from("Alice"),
+    };
+    person.say();
+}
