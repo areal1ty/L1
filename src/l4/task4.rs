5795741fc21de7f3440ad5d711e955e1d744bcdb
areality <areality.dt@gmail.com>
2024-09-18 00:05:15 +0300

    4'th task finished and tested

---
+++ src/l4/task4.rs
@@ -0,0 +1,48 @@
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc::{self, Receiver};
+use std::thread;
+use std::time::Duration;
+
+fn start_workers(num_workers: usize, rx: Arc<Mutex<Receiver<Option<i32>>>>) {
+    for id in 0..num_workers {
+        let rx = Arc::clone(&rx);
+        thread::spawn(move || loop {
+            let data = {
+                let rx = rx.lock().unwrap();
+                rx.recv().unwrap_or(None)
+            };
+
+            match data {
+                Some(value) => {
+                    println!("Worker {} received: {}", id, value);
+                    thread::sleep(Duration::from_millis(50));
+                }
+                None => {
+                    println!("Worker {}: no more data.", id);
+                    break;
+                }
+            }
+        });
+    }
+}
+
+pub fn run() {
+    let (tx, rx) = mpsc::channel();
+    let rx = Arc::new(Mutex::new(rx));
+
+    // Запуск воркеров
+    let num_workers = 4;
+    start_workers(num_workers, Arc::clone(&rx));
+
+    for i in 1..=10 {
+        tx.send(Some(i)).unwrap();
+        println!("Sent: {}", i);
+        thread::sleep(Duration::from_millis(100));
+    }
+
+    for _ in 0..num_workers {
+        tx.send(None).unwrap();
+    }
+
+    println!("All data sent.");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod l1;
 mod l2;
 mod l3;
+mod l4;
 
 use crate::l1::task1;
 use crate::l2::task2;
 use crate::l3::task3;
+use crate::l4::task4;
 
 fn main() {
     task1::run();
     task2::run(10);
-    task3::run(10)
+    task3::run(10);
+    task4::run();
 }
